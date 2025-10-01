d3145cc5d04d0732594c51bd498e278bd1e07685
root <root@NULL.localdomain>
2025-10-01 18:07:40 +0800

    Created a program to print out PC architechure.

---
+++ pc_stuff/pc_arch.sh
@@ -0,0 +1,21 @@
+echo "================================="
+echo	     "PC Architechure"
+echo "================================="
+
+echo "Hostname: $(hostname)"
+echo "Date: $(date)"
+echo "Uptime: $(uptime)"
+echo "Kernel: $(uname)"
+echo "Disk Usage:"
+df -h | grep '^/dev'
+echo "Memory Usage:"
+free -h
+echo "Number of users:"
+
+echo "CPU Info:"
+lscpu
+echo "Current Directory:"
+pwd
+echo "Current Partitions:"
+lsblk
+
