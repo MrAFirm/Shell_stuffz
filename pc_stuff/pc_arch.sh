636e5850d5d4fff6bf7f29aced861d7480364502
root <root@NULL.localdomain>
2025-10-01 22:55:01 +0800

    Did 3 mini-projects, pc_arch.sh to display your pc architechure, a barebones pomodoro timer using sleep, and read for welcome_name.

--- pc_stuff/pc_arch.sh
+++ pc_stuff/pc_arch.sh
@@ -6,16 +6,22 @@ echo "Hostname: $(hostname)"
 echo "Date: $(date)"
 echo "Uptime: $(uptime)"
 echo "Kernel: $(uname)"
+
 echo "Disk Usage:"
 df -h | grep '^/dev'
+
 echo "Memory Usage:"
 free -h
+
 echo "Number of users:"
+w -h | wc -l
 
 echo "CPU Info:"
 lscpu
+
 echo "Current Directory:"
 pwd
+
 echo "Current Partitions:"
 lsblk
 
---
+++ pomodoro/pomodoro_timer.sh
@@ -0,0 +1,8 @@
+while true
+do
+	echo "Pomodoro Timer Started! Focus!"
+	sleep 1500
+	echo "Time to Rest! Rest is Important!"
+	sleep 300
+	echo "Break's over, back to work!"
+done
